7daea114bdd7d564647fff2ad11e37a6e49f517c
ankit.mauni@marketwicks.com <bheemverma554@gmail.com>
2023-03-22 15:15:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const port=process.env.PORT||3000
+const io = require('socket.io')(http, {
+  cors: {
+    origins:'http://localhost:4200'
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hey Socket.io</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(process.env.PORT||3000, () => {
+  console.log('listening on *:3000');
+});
+
